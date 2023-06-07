b34a4a7b82d70e65b2a3d02436639c30dfc1799c
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-06-07 16:22:39 -0300

    feat: adicionando novas tarefas

--- scripts.js
+++ scripts.js
@@ -12,7 +12,22 @@ var todos = [
 const todolistApp = {
     data() {
         return {
-            todos: window.todos
+            todos: window.todos,
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if (this.newTodo.text) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+            } else {
+                alert("O texto da tarefa é obrigatório")
+            }
         }
     }
 };
