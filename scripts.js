5a04828c681052a0d81b7de899ff7b0600c3425f
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-06-07 19:27:22 -0300

    feat: implementada marcação de tarefas feitas, limpar tudo e refatorações no código

--- scripts.js
+++ scripts.js
@@ -1,18 +1,8 @@
-var todos = [
-    {
-        text: 'Aprender HTML, CSS e Javascript',
-        done: true
-    },
-    {
-        text: 'Aprender o básico de Vue JS',
-        done: false
-    }
-];
 
 const todolistApp = {
     data() {
         return {
-            todos: window.todos,
+            todos: [],
             newTodo: {
                 done: false
             }
